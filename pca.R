cef4fa4730504ca560d30341acf1369e24725474
Morgan Mendis <mmendis@aledade.com>
2016-10-16 14:38:19 -0400

    added some additional plots

--- pca.R
+++ pca.R
@@ -3,12 +3,16 @@ library(mice)
 library(reshape2)
 library(ggplot2)
 
+#Load the data used in the find_kmeans_cluster.R
+source('find_kmeans_cluster.R')
+
 #Select only numeric columns
 numeric_cols <- sapply(train, is.numeric)
 train.num <- train[,numeric_cols]
 
 #Remove constants and values with variance less than 10%
-train.num <- train.num[,apply(train.num, 2, var, na.rm=TRUE) < 0.1]
+train.num <- train.num[,apply(train.num, 2, var, na.rm=TRUE) > 0.1]
+train.num <- train.num[,apply(train.num, 2, var, na.rm=TRUE) != 0]
 
 #Create a function which calculates the missing values ratio
 missing.ratio <-function(x){
@@ -20,9 +24,10 @@ train.cln <- train.num[,apply(train.num, 2, missing.ratio) < 0.4]
 
 
 #What is the pattern regarding missing values in the data
+miss_pattern <- md.pattern(train.cln)
 miss <- data.frame(miss_pattern)
 
-#We see that we have 20029 records with complete values
+#We see that we have 38888 records with complete values
 #I am comfortable with that many records
 train.complete <- train.cln[complete.cases(train.cln),]
 
@@ -46,5 +51,10 @@ plot(train.pca, type = "l")
 #What are the descriptive statistics of each Principal Component
 cum_prop <- summary(train.pca)
 
-#variances of the number of variables
-std_dev <- train.pca$sdev
+#variances of the components
+pc.variance <- train.pca$sdev^2
+plot(pc.variance,type = 'l', xlab = 'Principal Component', ylab = 'Variance')
+
+#variance explained
+variance.explained <- train.pca$sdev^2 / sum(train.pca$sdev^2)
+plot(variance.explained,type = 'l', xlab = 'Principal Component', ylab = 'Variance Explained')

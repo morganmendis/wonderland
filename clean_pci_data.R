0bbac62f602fc51e05cf8703214b8e0b2e58c599
Morgan Mendis <mmendis@aledade.com>
2016-11-02 21:47:14 -0400

    changed some of the cleaning steps of the data

--- clean_pci_data.R
+++ clean_pci_data.R
@@ -1,18 +1,31 @@
 library(mice)
 library(reshape2)
 
+set.seed(240)
+
 # Read in the Charity Data
-train = read.csv("data/cup98LRN.txt",header = TRUE)
+raw = read.csv("data/cup98LRN.txt",header = TRUE)
+
+
+samp <- sample(nrow(raw), 0.6 * nrow(raw))
+
+train <- raw[samp, ]
+test <- raw[-samp, ]
+
+train_y <-train[,c("TARGET_B","TARGET_D")]
+
+x_vars <- colnames(train[!names(train) %in% c("TARGET_B","TARGET_D","CONTROLN")])
+train_x <- train[,x_vars]
+rm(x_vars)
 
 #Select only numeric columns
-numeric_cols <- sapply(train, is.numeric)
+numeric_cols <- sapply(train_x, is.numeric)
 train.num <- train[,numeric_cols]
 
-#Remove constants and values with variance less than 10%
-train.num <- train.num[,apply(train.num, 2, var, na.rm=TRUE) > 0.1]
+#Remove constants 
 train.num <- train.num[,apply(train.num, 2, var, na.rm=TRUE) != 0]
 
-source(clean_tools.R)
+source('clean_tools.R')
 
 #Select values which  have more than 60% of their records
 train.cln <- train.num[,apply(train.num, 2, missing.ratio) < 0.4]
